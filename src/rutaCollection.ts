dccbca362a092306cbd199890293875b31d2f4ff
Marco Barroso <alu0101386560@ull.edu.es>
2023-03-20 21:17:08 +0000

    nuevo commit

--- src/rutaCollection.ts
+++ src/rutaCollection.ts
@@ -1,19 +1,38 @@
 import {Ruta} from "./ruta"
 import {ID} from "./types";
+// import * as inquirer from "inquirer"
+
+
+/**
+ * Clase rutaCollection
+ */
 class rutaCollection {
+  /**
+   * Constructor por defecto
+   * @param coleccion_rutas_ array de rutas
+   */
   constructor(private coleccion_rutas_: Ruta[]) {
   }
 
+  /**
+   * getter coleccion_rutas_
+   */
   get getRutas(): Ruta[] {
     return this.coleccion_rutas_;
   } 
 
+  /**
+   * setter coleccion_rutas_
+   */
   set setRutas(coleccion_rutas: Ruta[]) {
     this.coleccion_rutas_ = coleccion_rutas;
   }
 
-  // añadir, borrar, modificar
-
+  /**
+   * Método que añade una nueva ruta a la coleción de rutas
+   * @param ruta ruta a añadir
+   * @returns array de rutas modificado o undefined en caso de no haber podido añadir la ruta.
+   */
   addRuta(ruta: Ruta): Ruta[] | undefined{
     let repetido = false;
     this.coleccion_rutas_.forEach((ruta_aux) => {
@@ -21,7 +40,6 @@ class rutaCollection {
         repetido = true;
       }
     });
-
     if (repetido === false) {
       this.coleccion_rutas_.push(ruta);
       return this.coleccion_rutas_;
@@ -30,10 +48,13 @@ class rutaCollection {
       return undefined;
       
     }
-    
   }
 
-
+  /**
+   * Método que borra una ruta
+   * @param identificador identificador de la ruta a borrar
+   * @returns ruta eliminada o undefined en caso de que no existiera.
+   */
   borrarRuta(identificador: ID): Ruta | undefined{
     let ruta_aux: Ruta; 
     this.coleccion_rutas_.forEach((ruta, indice) => {
@@ -52,6 +73,23 @@ class rutaCollection {
 
 
 
+
+
+
+//   function promptUser(): void {
+//     console.clear();
+//     displayTodoList();
+//     inquirer.prompt({
+//             type: "list",
+//             name: "command",
+//             message: "Choose option",
+//             choices: Object.values(Commands)
+//     }).then(answers => {
+//         if (answers["command"] !== Commands.Quit) {
+//             promptUser();
+//         }
+//     })
+// }
 
 
 }
